c6b524cd42867902b377d418bbc82938bacae8f0
Chinmay Shet <chishxd@gmail.com>
2026-09-13 21:35:43 +0530

    refactor: set() accepts the file instead of creating a file

--- src/main.zig
+++ src/main.zig
@@ -13,11 +13,8 @@ const Header = extern struct {
 };
 
 ///set functions stores the binary payload passed with key and value with a Header to identify the value
-fn set(io: std.Io, key: []const u8, value: []const u8) !void {
+fn set(io: std.Io, file: std.Io.File, key: []const u8, value: []const u8) !void {
     const header = Header{ .magic = MAGIC, .key_len = @intCast(key.len), .value_len = @intCast(value.len) };
-    const file = try std.Io.Dir.cwd().createFile(io, "test_db.db", .{ .truncate = false });
-    defer file.close(io);
-
     //Initializing buffer to write into the file
     var buffer: [1024]u8 = undefined;
     var file_writer = file.writer(io, &buffer);
@@ -37,18 +34,20 @@ pub fn main(init: std.process.Init) !void {
     const value: []const u8 = "goku";
 
     const io = init.io;
+    const file = try std.Io.Dir.cwd().createFile(io, "test_db.db", .{ .truncate = false });
+    defer file.close(io);
 
-    try set(io, key, value);
+    try set(io, file, key, value);
     std.debug.print("Successfully Stored key-value in Database\n", .{});
 }
 
 test "set writes record to disk" {
     const io = std.testing.io;
 
-    try set(io, "hello", "world");
-
-    var file = try std.Io.Dir.cwd().openFile(io, "test_db.db", .{});
+    const file = try std.Io.Dir.cwd().createFile(io, "test_db.db", .{ .truncate = false });
     defer file.close(io);
+    defer std.Io.Dir.cwd().deleteFile(io, "test_db.db") catch {};
+    try set(io, file, "hello", "world");
 
     const file_len = try file.length(io);
     try std.testing.expectEqual(@as(u64, 22), file_len);

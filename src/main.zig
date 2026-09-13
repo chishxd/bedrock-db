79db74e63f44408bc3303d41a993b6bc32ef83d2
Chinmay Shet <chishxd@gmail.com>
2026-09-14 02:33:10 +0530

    feat(get): Read value back from database
    So this took quite a while for me to understand
    as Allocators are entirely new concepts for me hehe.
    What I did was get header info in a buffer, then read the key according
    to the length info stored in header.. And if key matched, allocate the
    memory for value and return it back to the caller

--- src/main.zig
+++ src/main.zig
@@ -29,12 +29,40 @@ fn set(io: std.Io, file: std.Io.File, key: []const u8, value: []const u8) !void 
     try file_writer.flush();
 }
 
+fn get(io: std.Io, file: std.Io.File, key: []const u8, allocator: std.mem.Allocator) !?[]u8 {
+    var read_buf: [1024]u8 = undefined;
+    var file_reader = file.reader(io, &read_buf);
+
+    try file_reader.seekTo(0);
+
+    while (true) {
+        var header: Header = undefined;
+        try file_reader.interface.readSliceAll(std.mem.asBytes(&header));
+
+        if (header.magic != MAGIC) {
+            return error.InvalidHeader;
+        }
+
+        var key_buf: [256]u8 = undefined;
+        const key_slice = key_buf[0..header.key_len];
+        try file_reader.interface.readSliceAll(key_slice);
+
+        if (std.mem.eql(u8, key, key_slice)) {
+            const value_buf = try allocator.alloc(u8, header.value_len);
+            try file_reader.interface.readSliceAll(value_buf);
+            return value_buf;
+        } else {
+            try file_reader.seekTo(header.value_len);
+        }
+    }
+}
+
 pub fn main(init: std.process.Init) !void {
     const key: []const u8 = "user";
     const value: []const u8 = "goku";
 
     const io = init.io;
-    const file = try std.Io.Dir.cwd().createFile(io, "test_db.db", .{ .truncate = false });
+    const file = try std.Io.Dir.cwd().createFile(io, "test_db.db", .{ .truncate = false, .read = true });
     defer file.close(io);
 
     try set(io, file, key, value);
